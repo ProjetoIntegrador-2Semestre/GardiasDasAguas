644fae3b8451fb6b1b1bc87c8b7551b8a0dfd385
Felipe C. <felipecutiurdossantos@gmail.com>
2025-10-29 20:55:28 -0300

    :ok_hand: Style: Code review, and adjustments.

--- frontend-gda/src/app/Components/LoginCard.tsx
+++ frontend-gda/src/app/Components/LoginCard.tsx
@@ -3,7 +3,7 @@ import Button from "./UI/Button"
 
 export default function LoginCard(){
     return(
-        <div className="bg-white/10 backdrop-blur-[4px] border-white border-1 w-[70vh] h-[50vh] flex flex-col rounded-2xl">
+        <div className="bg-white/10 backdrop-blur-[4px] border-white border-1 w-[70vh] h-[60vh] flex flex-col rounded-2xl">
             <div className="items-center flex flex-col p-10 gap-10">
                   <h1 className="text-white">Bem vindo de volta!</h1>
             <input className="bg-white rounded-4xl  placeholder-black/40 w-[50vh] p-3 "type="text" placeholder="Insira seu email." />
@@ -15,12 +15,12 @@ export default function LoginCard(){
                 <a href="" className="text-white/50">Esqueci a senha.</a>
             </div>
 
-            <div className="flex gap-3 pl-28 pt-5 items-center">
+            <div className="flex gap-3 pl-28 pt-10 items-center">
                 <input type="checkbox" className="h-[3vh] w-[3vh] rounded-2xl border-none" />
                 <p className="text-white">Lembrar-me a senha.</p>
             </div>
 
-            <div className="justify-center items-center flex gap-5 pt-5">
+            <div className="flex justify-center items-center gap-5 pt-10">
                 <Button nome={"Login"} estilo={"login"} clique={function (): void {
                     throw new Error("Function not implemented.")
                 } }></Button>
