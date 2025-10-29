82e1c29c1f15c5f0f5e0d934d3508022c357994c
ryan <148487663+RyanAugustoDeOliveira@users.noreply.github.com>
2025-10-29 19:10:36 -0300

    :bug: fix: Button update

--- frontend-gda/src/app/Components/UI/Button.tsx
+++ frontend-gda/src/app/Components/UI/Button.tsx
@@ -8,10 +8,10 @@ import { error } from "console";
     } 
 
         const estilos = {
-            login: "w-[14vh] h-[5vh] bg-[#FF62C8] transition duration-300 hover:scale-110 text-white",
-            cadastro: "w-[18vh] h-[5vh] bg-white transition duration-300 hover:scale-110 text-[#FF62C8] border-2 border-[#FF62C8]",
-            conhecer: "w-[27vh] h-[7vh] bg-[#FF62C8] transition duration-300 hover:scale-110 text-white",
-            LerMais: "w-[20vh] h-[5vh] bg-[#FF62C8] transition duration-300 hover:scale-110 text-white border-2 border-white",
+            login: "w-[7vw] h-[5vh] bg-[#FF62C8] transition duration-300 hover:scale-110 text-white",
+            cadastro: "w-[9vw] h-[5vh] bg-white transition duration-300 hover:scale-110 text-[#FF62C8] border-2 border-[#FF62C8]",
+            conhecer: "w-[14vw] h-[7vh] bg-[#FF62C8] transition duration-300 hover:scale-110 text-white",
+            LerMais: "w-[8.6vw] h-[5vh] bg-[#FF62C8] transition duration-300 hover:scale-110 text-white border-2 border-white",
         } as const
 
     export default function Button({nome, estilo, clique}: IBotao){
